a5cc2e107a03c2d996d112ae7e1022d2ac30fde4
jpcrisostomo <62001546+jpcrisostomo@users.noreply.github.com>
2022-07-08 23:15:32 -0400

    Update app.js
    prueba para pantallazo

--- public/app.js
+++ public/app.js
@@ -4,39 +4,33 @@
 // let URL_Linode = "http://45.33.35.67/mediciones/";
 
 async function getMediciones() {
-  let URL_Mediciones = "http://localhost/mediciones/";  
-  
+  let URL_Mediciones = "http://45.33.35.67/mediciones/";  
+
   try {
       const res = await fetch(URL_Mediciones); 
       const datillos = await res.json();
-      const { ecg1,ecg2,ecg3,ecg4,ecg5,ecg6,ecg7,ecg8,ecg9,ecg10,ecg11,ecg12,ecg13,ecg14,ecg15,ecg17,ecg18,ecg19,ecg20, tmp, oxi, resp, fcard, btn1, btn2, btn3, btn4  } = datillos;
+      const { ecg, tmp, oxi, resp, fcard, btn1, btn2, btn3, btn4  } = datillos;
       //Cargar datos ECG a chart js
-      ECGs = [ecg1,ecg2,ecg3,ecg4,ecg5,ecg6,ecg7,ecg8,ecg9,ecg10,ecg11,ecg12,ecg13,ecg14,ecg15,ecg17,ecg18,ecg19,ecg20]; 
-      const d = new Date();
 
-      document.getElementById('temperatura').textContent = tmp;
-      document.getElementById('oxigenacion').textContent = oxi;
-      document.getElementById('f_respiratoria').textContent = resp;
-      document.getElementById('f_cardiaca').textContent = fcard;
-      if(btn1=="1"||btn1==1){
-        document.getElementById('b1').textContent = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-      }
-    if(btn2=="1"||btn2==1){
-        document.getElementById('b2').textContent = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-      }
-    if(btn3=="1"||btn3==1){
-        document.getElementById('b3').textContent = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-      }
-    if(btn4=="1"||btn4==1){
-        document.getElementById('b4').textContent = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-      }
+      passECG(ecg); 
+
+      document.getElementById('temperatura').textContent = 36.5;
+      document.getElementById('oxigenacion').textContent = 91;
+      document.getElementById('f_respiratoria').textContent = 10.1;
+      document.getElementById('f_cardiaca').textContent = 72;
+        document.getElementById('b1').textContent = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        document.getElementById('b2').textContent = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        document.getElementById('b3').textContent = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        document.getElementById('b4').textContent = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  
   } catch (error) {  
       console.log(error);
   }
 }
 
+/* Funcion que controla que tan rapido se llama a pedir datos desde server
+*/
 setInterval(getMediciones,1000);
-setInterval(actualizeECGindex,55);
 
 function init() {
 dataPlot = new Chart(document.getElementById("line-chart"), {
@@ -53,28 +47,19 @@ dataPlot = new Chart(document.getElementById("line-chart"), {
 });
 }
 
-
-
-var ECGs = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]; 
 var dataPlot;
 var maxDataPoints = 140;
-var ECG_index=0;
 
-function actualizeECGindex()
-{
-  passECG(ECGs[ECG_index]);
-  ECG_index ++;
-  if (ECG_index ==20){
-    ECG_index=0;
-  }
-}
-
+/* ACA EDITAR ,TOMMY, por cada 1 get mediciones se tiene que hacer un loop que recorra cada
+// entrada del vector (json) y meta a addData cada contenido de "ecg" */
 function passECG(ecg) {
   console.log(ecg);
   var today = new Date();
   var t = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
   addData(t, ecg);
 }
+
+
 
 function removeData(){
 dataPlot.data.labels.shift();
